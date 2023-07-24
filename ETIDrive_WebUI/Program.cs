6d08f9a12372c352ff06fcecde8448af41c6cff0
Berk <berkkarakas98@hotmail.com>
2023-07-24 15:29:39 +0300

    Configure ETIDriveContext with Development connection string

--- ETIDrive_WebUI/Program.cs
+++ ETIDrive_WebUI/Program.cs
@@ -1,8 +1,23 @@
+using ETIDrive_Data;
+using ETIDrive_Entity.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Service container.
 builder.Services.AddControllersWithViews();
 
+// Configures ASP.NET Core Identity for the application with Entity Framework for data storage.
+builder.Services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<ETIDriveContext>();
+
+// Development connection string
+string path = Directory.GetCurrentDirectory();
+var connectionString = builder.Configuration.GetConnectionString("Development")?.Replace("[DataDirectory]", path);
+
+// Using the SQL Server database with the provided connection string.
+builder.Services.AddDbContext<ETIDriveContext>(options =>
+    options.UseSqlServer(connectionString));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
